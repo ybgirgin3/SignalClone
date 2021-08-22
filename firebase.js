e823fe377dba265ae83900834dc3c3ecc9b2772e
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2021-08-22 22:03:57 +0300

    deployed

--- firebase.js
+++ firebase.js
@@ -4,13 +4,23 @@ import "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAG3RKFLLCXZjBus4Sq5KlekWiV8W6z2Co",
-  authDomain: "signal-after-quota.firebaseapp.com",
-  projectId: "signal-after-quota",
-  storageBucket: "signal-after-quota.appspot.com",
-  messagingSenderId: "864821748275",
-  appId: "1:864821748275:web:0c277263dedd032363b946"
+  apiKey: "AIzaSyBohzwtW-u0_OyCKcwrTi1--UPzlwyfoYA",
+  authDomain: "second-after-quota.firebaseapp.com",
+  projectId: "second-after-quota",
+  storageBucket: "second-after-quota.appspot.com",
+  messagingSenderId: "939325109585",
+  appId: "1:939325109585:web:0594c66b1fcd0902cb4f70"
 };
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAG3RKFLLCXZjBus4Sq5KlekWiV8W6z2Co",
+//   authDomain: "signal-after-quota.firebaseapp.com",
+//   projectId: "signal-after-quota",
+//   storageBucket: "signal-after-quota.appspot.com",
+//   messagingSenderId: "864821748275",
+//   appId: "1:864821748275:web:0c277263dedd032363b946"
+// };
 // const firebaseConfig = {
 //   apiKey: "AIzaSyCieMEJOsPhb_CfNVdv5Uf80u4MdvX-8i4",
 //   authDomain: "fun-signal-clone.firebaseapp.com",
